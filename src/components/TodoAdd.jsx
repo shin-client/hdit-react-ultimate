0d498a60930925ad2747a88e0ce55a66145a73e0
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-06-06 08:28:12 +0700

    done todo app

--- src/components/TodoAdd.jsx
+++ src/components/TodoAdd.jsx
@@ -1,14 +1,31 @@
-const TodoAdd = () => {
+import { useState } from "react";
+
+const TodoAdd = ({addNewTodo}) => {
+  const [valueInput, setValueInput] = useState("");
+
+  const handleAddTodo = () => {
+    if (valueInput.trim() !== "") {
+      addNewTodo(valueInput);
+      setValueInput("");
+    }
+  };
+
   return (
     <div className="space-x-10">
       <input
         type="text"
+        value={valueInput}
         className="bg-gray-100 outline-none rounded px-4 py-2"
         placeholder="Enter your task"
+        onChange={(e) => setValueInput(e.target.value)}
       />
-      <button className="bg-blue-400 px-4 py-2 text-white rounded cursor-pointer hover:opacity-85 transition-colors">
+      <button
+        className="bg-blue-400 px-4 py-2 text-white rounded cursor-pointer hover:opacity-85 transition-colors"
+        onClick={handleAddTodo}
+      >
         Add
       </button>
+      <p>My value is: {valueInput}</p>
     </div>
   );
 };
