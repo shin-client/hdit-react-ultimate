7ff32c91f1bc14911b6a80a777a603014954484c
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-06-06 08:53:44 +0700

    feat: implement page components for routing (Login, Register, Users, Products)

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import UsersPage from "./pages/UsersPage.jsx";
+import ProductsPage from "./pages/ProductsPage.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
-  { path: "/login", element: <div>This is login page</div> },
-  { path: "/register", element: <div>This is register page</div> },
-  { path: "/users", element: <div>This is users page</div> },
-  { path: "/products", element: <div>This is products page</div> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/register", element: <RegisterPage /> },
+  { path: "/users", element: <UsersPage /> },
+  { path: "/products", element: <ProductsPage /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
